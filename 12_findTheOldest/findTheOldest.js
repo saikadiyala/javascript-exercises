a22674304f69aab3cb2665565560463b17a49fbc
Sai Kadiyala <saikadiyala201@gmail.com>
2024-01-31 11:33:56 -0600

    Worked on 08,09,11,12 problems

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let k=people.map((item)=>{
+        let age;
+        if(item.yearOfDeath){
+            age=item.yearOfDeath-item.yearOfBirth;
+        }
+        else{
+            let currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
+            age= currentYear-item.yearOfBirth;
+        }
+        let s={};
+        s['name']= item.name;
+        s['age']=age;
+        return s; 
+    });
+
+    
+    let finding_lowest= k.reduce((i,item)=>{return (i<item.age)?i=item.age:i;},0);
+    let finding_old= k.find(item=>item.age===finding_lowest);
+    console.log(finding_old);
+    let min={};
+    people.forEach(element => {
+        if(element.name===finding_old.name){
+            min=element;
+        }
+    });
+    return min;
 
 };
 
